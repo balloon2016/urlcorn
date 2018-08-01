8b76643c5a53c3d01f11bfa4624e7db4e4be032e
purk <kimdonghyun0276@gmail.com>
2018-08-01 23:31:07 +0900

    purk
    - Page Design Modifided.
    - Design Script Modified.
    - Typed Fix.

--- login/main.js
+++ login/main.js
@@ -1,3 +1,12 @@
+window.onload=function() {
+	var a=document.getElementsByClassName('dropbtn');
+	var b=document.getElementsByClassName('dropdown-content');
+	var c;
+	for(c=0; c<a.length; c++) {
+		b[c].style.width=a[c].clientWidth+"px";
+	}
+}
+
 window.onclick = function(e) {
 	if(e.path[0].className=="dropbtn") {
 		var c, k=document.getElementsByClassName('dropdown-content');
@@ -5,8 +14,8 @@ window.onclick = function(e) {
 		for(c=0; c<k.length; c++) {
 			if(k[c]==dropdowns) 
 				switch(k[c].classList.contains("show")) {
-						case true: k[c].classList.remove("show"); break;
-						case false: k[c].classList.add("show"); break;
+					case true: k[c].classList.remove("show"); break;
+					case false: k[c].classList.add("show"); break;
 				}
 			else 
 				k[c].classList.remove("show");
